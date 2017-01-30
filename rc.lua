7710067436f5ce7a427c7b586a13afb8493351a3
Jaime Machado <jaime.machado.neto@hp.com>
2017-01-30 09:58:29 +0100

    Initial work config

--- rc.lua
+++ rc.lua
@@ -766,6 +766,9 @@ awful.rules.rules = {
     { rule = { class = "Chromium-browser" },   properties = { floating = false } },
     { rule = { class = "Google-chrome" },   properties = { floating = false } },
     { rule = { class = "Firefox" },   properties = { floating = false } },
+--setup project
+    { rule = { class = "setupGUI" },   properties = { floating = true } },
+
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
