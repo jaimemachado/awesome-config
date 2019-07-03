ee08231dd701a2f099fc4ca9c0d4630a39ab3575
Jaime Machado <jaime.machado.neto@hp.com>
2019-07-03 11:16:16 +0200

    Adding new floatting configuration

--- rc.lua
+++ rc.lua
@@ -117,18 +117,22 @@ revelation.init()
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.floating
+      awful.layout.suit.tile,
+      awful.layout.suit.tile.left,
+      awful.layout.suit.tile.bottom,
+      awful.layout.suit.tile.top,
+      awful.layout.suit.fair,
+      awful.layout.suit.fair.horizontal,
+      awful.layout.suit.spiral,
+      awful.layout.suit.spiral.dwindle,
+      awful.layout.suit.max,
+      awful.layout.suit.max.fullscreen,
+      awful.layout.suit.magnifier,
+      awful.layout.suit.floating,      
+      -- awful.layout.suit.corner.nw,
+      -- awful.layout.suit.corner.ne,
+      -- awful.layout.suit.corner.sw,
+      -- awful.layout.suit.corner.se
 }
 -- }}}
 
@@ -854,6 +858,8 @@ awful.rules.rules = {
     { rule = { class = "sky" }, properties = { floating = true } },
     { rule = { class = "omestools" }, properties = { floating = true } },
     { rule = { class = "Omestools" }, properties = { floating = true } },
+    { rule = { class = "kyber" }, properties = { floating = true } },
+    { rule = { class = "Kyber" }, properties = { floating = true } },    
     { rule = { class = "Sky" }, properties = { floating = true } },
     { rule = { class = "galculator" }, properties = { floating = true } },
     { rule = { class = "Galculator" }, properties = { floating = true } },
